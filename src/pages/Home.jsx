394c0bf443faa8a8a3d3a134fe111d5a70462a98
rt4914 <rajattalesra4914@gmail.com>
2024-07-15 18:21:28 +0530

    showing trending movies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../sections/Hero"
+import Trending from "../sections/Trending"
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Trending />
     </div>
   )
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -12,11 +12,11 @@ const Hero = () => {
       <Carousel stopOnHover={true} showThumbs={false} showStatus={false}>
         { heroData && heroData.map((movie) => {
           return(
-            <div key={movie.id} className="h-screen">
-              <img className="w-full h-screen" src={imageBaseUrl + movie.backdrop_path} />
-              <div className="z-10 absolute bottom-20 px-12 flex flex-col items-start">
-                <h2 className="text-5xl font-medium text-white">{movie.title}</h2>
-                <p className="mt-8 text-base text-gray-100 text-start lg:me-96">{movie.overview}</p>
+            <div key={movie.id}>
+              <img className="w-full md:h-screen" src={imageBaseUrl + movie.backdrop_path} />
+              <div className="z-10 absolute bg-gradient-to-b from-transparent to-black bottom-0 pb-12 md:pb-20 lg:px-12 md:px-8 px-4 flex flex-col items-start">
+                <h2 className="text-xl md:text-5xl font-medium text-white text-start">{movie.title}</h2>
+                <p className="hidden md:flex mt-8 text-base text-gray-100 text-start lg:me-96">{movie.overview}</p>
                 <span className="flex items-center gap-2 mt-4">
                   <FaStar />
                   { Number(movie.vote_average).toFixed(1)}/10
