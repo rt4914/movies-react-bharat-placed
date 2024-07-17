98135b85f02b4c1e293ee873537f6462b36a080e
rt4914 <rajattalesra4914@gmail.com>
2024-07-17 11:06:57 +0530

    added image lazy loading

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,34 @@
 import { FaStar } from "react-icons/fa";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import { useSelector } from "react-redux"
 import { Link } from "react-router-dom";
+import PlaceholderImage from '../assets/placeholder.png'
+import LoadingImage from '../assets/loading.jpg'
+import { useState } from "react";
 
 const MovieCard = ({movie}) => {
   const imageBaseUrl = useSelector((state) => state.movies.imageBaseUrl)
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setIsLoading(false);
+  };
+
+  const handleImageError = (event) => {
+    event.target.src = PlaceholderImage;
+  };
 
   return (
     <Link to={`movie/${movie.id}`}>
       <div className="w-[320px] rounded shadow-md relative">
-        <img className="w-full h-full rounded" src={imageBaseUrl + movie.backdrop_path} />
+        <LazyLoadImage
+          className="w-[320px] h-[180px] rounded"
+          alt={PlaceholderImage.alt}
+          height={PlaceholderImage.height}
+          src={isLoading ? LoadingImage : imageBaseUrl + movie.backdrop_path}
+          onLoad={handleImageLoad}
+          onError={handleImageError}
+          width={PlaceholderImage.width} />
         <div className="w-full z-10 absolute bg-gradient-to-b from-transparent to-black bottom-0 p-2 flex flex-col items-start">
           <h2 className="font-medium text-white text-lg">{movie.title}</h2>
           <span className="flex items-center gap-2 mt-0 text-sm">
